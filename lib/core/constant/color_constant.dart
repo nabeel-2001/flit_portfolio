233e2eb3b765bd7d0768b2b39deda5919a24362b
smijithnvasudev <smijithnvasudev@gmail.com>
2024-10-02 14:43:40 +0530

    color palatte added

--- lib/core/constant/color_constant.dart
+++ lib/core/constant/color_constant.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 class ColorPalette {
-
-  static Color primary = const Color(0xffFFFFFF);
+  static const Color primaryColor = const Color(0xff7f04cc);
+  static const Color secondaryColor = const Color(0xffFFFFFF);
+  static const Color thirdColor = const Color(0xffFFFFFF);
 }
