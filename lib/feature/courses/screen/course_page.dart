7a0f02242ed989a60ccd21f5325d8e6814cdcf48
Nabeel <mohammednabeel940@gmail.com>
2024-10-02 14:27:31 +0530

    first file structure added2

---
+++ lib/feature/courses/screen/course_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CoursePage extends StatefulWidget {
+  const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
